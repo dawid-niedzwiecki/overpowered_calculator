fd750cfafd335fc4ebc90064513f6e6ddf8554ae
dawid-niedzwiecki <dawid.niedwiecki@gmail.com>
2021-11-10 21:19:11 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+using 🏉 = string;
+
+#define 👈 <<
+#define 👉 >>
+#define 🎹 cin
+#define 🔢 int
+#define 🐮 main()
+#define 🔙 return
+#define 💻 cout
+#define 🕛 0
+#define 🕐 1
+#define 🕑 2
+#define 🕒 3
+#define 🕓 4
+#define 🕔 5
+#define 🕕 6
+#define 🕖 7
+#define 🕗 8
+#define 🕘 9
+#define 📧 message
+#define 💨 endl
+
+🔢 🐮 {
+	🏉 📧;
+    🎹 👉 📧;
+	💻 👈 📧 👈 💨;
+	🔢 💚;
+	🎹 👉 💚;
+	💻 👈 💚 + 🕘 👈 💨;
+    🔙 🕛;
+}
